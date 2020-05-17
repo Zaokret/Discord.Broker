8c103aeb393b922bd7c99755d554c6d0e7c5559d
Lazar Todorovic <ltodorovic@teksavvy.ca>
2020-05-17 18:51:16 -0400

    removed token

--- DiscordBot.Core/Configuration/Configuration.cs
+++ DiscordBot.Core/Configuration/Configuration.cs
@@ -8,7 +8,6 @@ namespace DiscordBot
   public class Configuration
   {
     public string Token { get; set; }
-    public ulong MainGuild { get; set; }
     public IEnumerable<Coin> Coins { get; set; }
     public char CommandPrefix { get; set; }
   }
